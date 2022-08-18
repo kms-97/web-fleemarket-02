79c2e9653afe963c28e57d6823beb2479b1202fc
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-18 21:20:05 +0900

    feat: 생성한 Entity 파일 TypeOrmModule에 주입
    - #6

--- packages/server/src/category/category.module.ts
+++ packages/server/src/category/category.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { Category } from './entities/category.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService],
-  exports: [],
 })
 export class CategoryModule {}

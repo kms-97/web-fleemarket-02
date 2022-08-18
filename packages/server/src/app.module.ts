c402068823419b1fcbac3f8fbde29dde7e4333f6
kms-97 <msg016@naver.com>
2022-08-18 20:42:02 +0900

    feat: typeORM 데이터베이스 연동

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from '@category/category.module';
 import { DatabaseModule } from '@database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     DatabaseModule,
+    CategoryModule,
   ],
 })
 export class AppModule {}
--- packages/server/src/database/database.module.ts
+++ packages/server/src/database/database.module.ts
@@ -1,9 +1,24 @@
-import { Global, Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Global()
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        port: configService.get('DB_PORT'),
+        database: configService.get('DB_NAME'),
+        //entities: ['dist/**/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}

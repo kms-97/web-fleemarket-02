e0a35781250bd82fd4e266074861093ed47d8cdd
kms-97 <msg016@naver.com>
2022-08-20 03:16:06 +0900

    feat: 예외 처리 필터 추가 - 코드 내에서 발생하는 모든 httpException과 customException에 대해 success와 message를 응답에 추가함

---
+++ packages/server/src/filter/HttpException.filter.ts
@@ -0,0 +1,23 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { CustomException } from '@src/base/CustomException';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException | CustomException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const { message } = exception.getResponse() as { message: string[] };
+
+    response.status(status).json({
+      success: false,
+      message: message ?? exception.message,
+    });
+  }
+}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filter/HttpException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,7 @@ async function bootstrap() {
     },
   });
   app.setGlobalPrefix('/api');
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,

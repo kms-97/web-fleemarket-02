725d86b8fba71087def9a34789f84a5c9def5644
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-19 11:11:28 +0900

    feat: 이미지 업로드 최대 개수 설정

--- packages/server/src/image/image.controller.ts
+++ packages/server/src/image/image.controller.ts
@@ -9,7 +9,7 @@ import { FilesInterceptor } from '@nestjs/platform-express';
 @Controller('image')
 export class ImageController {
   @Post()
-  @UseInterceptors(FilesInterceptor('image'))
+  @UseInterceptors(FilesInterceptor('image', 10))
   async uploadImages(@UploadedFiles() files: Express.MulterS3.File[]) {
     return files.reduce((arr, image) => {
       arr.push(image.location);

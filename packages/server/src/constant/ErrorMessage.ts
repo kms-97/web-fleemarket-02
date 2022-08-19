6ad2bafe1a937c074cd1f0b087c577214f697778
kms-97 <msg016@naver.com>
2022-08-20 03:15:33 +0900

    feat: 예외 메세지 상수화

---
+++ packages/server/src/constant/ErrorMessage.ts
@@ -0,0 +1,10 @@
+export const ErrorMessage = {
+  NOT_VALID_FORMAT: '잘못된 형식의 요청입니다.',
+  NOT_EMAIL_FORMAT: '잘못된 이메일 형식입니다.',
+  NOT_FOUND_USER: '해당하는 유저를 찾을 수 없습니다',
+  DUPLICATED_USER_ID: '이미 존재하는 ID입니다.',
+  NEED_ONE_SEARCH_CONDITION: '검색 조건이 하나 이상 존재해야 합니다.',
+  EXCEED_ONE_SEARCH_CONDITION: '검색 조건은 하나만 가능합니다.',
+  EXCEED_USER_LOCATION_LIMIT: '관심 지역은 최대 2개까지 설정 가능합니다.',
+  NOT_EMPTY_USER_LOCATION: '관심 지역은 최소 1개 설정되어야 합니다.',
+};

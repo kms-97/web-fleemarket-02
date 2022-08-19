0cfda0c8ad1dbe03affd9e7702ff7e7205f7e76b
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-19 11:04:13 +0900

    feat: image upload 로직 생성
    - #8

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from '@category/category.module';
 import { DatabaseModule } from '@database/database.module';
+import { ImageModule } from './image/image.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     DatabaseModule,
     CategoryModule,
+    ImageModule,
   ],
 })
 export class AppModule {}
---
+++ packages/server/src/image/image.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+
+@Controller('image')
+export class ImageController {
+  @Post()
+  @UseInterceptors(FilesInterceptor('image'))
+  async uploadImages(@UploadedFiles() files: Express.MulterS3.File[]) {
+    return files.reduce((arr, image) => {
+      arr.push(image.location);
+      return arr;
+    }, []);
+  }
+}

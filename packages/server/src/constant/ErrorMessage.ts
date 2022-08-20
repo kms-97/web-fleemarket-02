5a8b204e12bbe15c021048b49ba701f31fdd554a
kms-97 <msg016@naver.com>
2022-08-20 19:34:48 +0900

    feat: 필수 항목 에러메세지 추가

--- packages/server/src/constant/ErrorMessage.ts
+++ packages/server/src/constant/ErrorMessage.ts
@@ -2,6 +2,7 @@ export const ErrorMessage = {
   NOT_VALID_FORMAT: '잘못된 형식의 요청입니다.',
   NOT_EMAIL_FORMAT: '잘못된 이메일 형식입니다.',
   NOT_FOUND_USER: '해당하는 유저를 찾을 수 없습니다',
+  NOT_FOUND_ESSENTIAL: '필수 항목이 누락되었습니다',
   DUPLICATED_USER_ID: '이미 존재하는 ID입니다.',
   NEED_ONE_SEARCH_CONDITION: '검색 조건이 하나 이상 존재해야 합니다.',
   EXCEED_ONE_SEARCH_CONDITION: '검색 조건은 하나만 가능합니다.',

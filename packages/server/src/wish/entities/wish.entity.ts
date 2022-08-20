9e8c75982513362d63a311fdf23d974021aefa62
kms-97 <msg016@naver.com>
2022-08-21 00:03:09 +0900

    feat: wish 레포지토리 추가

--- packages/server/src/wish/entities/wish.entity.ts
+++ packages/server/src/wish/entities/wish.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from '@src/base/BaseEntity';
 import { Product } from '@src/product/entities/product.entity';
 import { User } from '@src/user/entities/user.entity';
-import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, Repository } from 'typeorm';
 
 @Entity({ name: 'Wish' })
 export class Wish extends BaseEntity {
@@ -13,3 +13,5 @@ export class Wish extends BaseEntity {
   @JoinColumn({ name: 'product_id' })
   productId!: number;
 }
+
+export type WishRepository = Repository<Wish>;

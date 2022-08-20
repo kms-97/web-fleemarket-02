af75b51c339c31112f422830e287e34202aed286
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-20 23:14:06 +0900

    feat: User 인증 관련 메서드가 정의된 AuthService 생성
    - #13

---
+++ packages/server/src/auth/auth.service.ts
@@ -0,0 +1,115 @@
+import { verify } from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserService } from '@user/user.service';
+import { ConfigService } from '@nestjs/config';
+
+import { encryptValue } from '@utils/crypto';
+
+import { Auth, AuthRepository } from './entities/auth.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    @InjectRepository(Auth) private readonly authRepository: AuthRepository,
+  ) {}
+
+  async login(userId: number) {
+    const payload = { id: userId };
+
+    const accessToken = this.jwtService.sign(payload, { expiresIn: '5m' });
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: '14d',
+    });
+
+    const isExist = await this.isExistRefreshTokenByUserId(userId);
+
+    if (isExist) {
+      await this.updateRefreshToken(userId, refreshToken);
+    } else {
+      await this.insertRefreshToken(userId, refreshToken);
+    }
+
+    const enKey = this.configService.get('LOGIN_SECRET_KEY');
+
+    return [
+      encryptValue(accessToken, enKey),
+      encryptValue(refreshToken, enKey),
+    ];
+  }
+
+  async validateUser(userId: string, password: string) {
+    const user = await this.userService.getUserWithHashPasswordByUserId(userId);
+    const isCompare = await this.userService.comparePassword(
+      password,
+      user?.password ?? '',
+    );
+
+    if (!user || !isCompare) {
+      return null;
+    }
+
+    const result = await this.userService.getUserById(user.id);
+
+    return result;
+  }
+
+  async verifyRefresh(aRefreshToken: string) {
+    const row = await this.authRepository.query(
+      `
+      select a.refresh_token as refreshToken
+      from Auth a
+      where a.refresh_token = ?
+      `,
+      [aRefreshToken],
+    );
+
+    if (!row[0]) {
+      return false;
+    }
+
+    const result = verify(
+      row[0].refreshToken,
+      this.configService.get('JWT_SECRET_KEY'),
+    );
+
+    return Boolean(result);
+  }
+
+  async isExistRefreshTokenByUserId(userId: number) {
+    const row = await this.authRepository.query(
+      `
+      select a.refresh_token as refreshToken
+      from Auth a 
+      where a.user_id = ?
+      `,
+      [userId],
+    );
+
+    return Boolean(row[0]);
+  }
+
+  async updateRefreshToken(userId: number, refreshToken: string | null) {
+    return await this.authRepository.query(
+      `
+        update Auth set refresh_token = "${refreshToken}"
+        where user_id = ?
+        `,
+      [userId],
+    );
+  }
+
+  async insertRefreshToken(userId: number, refreshToken: string) {
+    return await this.authRepository.query(
+      `
+        insert into Auth (user_id, refresh_token) 
+        values (?, ?)
+        `,
+      [userId, refreshToken],
+    );
+  }
+}

f7b21a0564f2bde96650fa225550753c2b139464
kms-97 <msg016@naver.com>
2022-08-20 19:35:38 +0900

    feat: product 엔티티 validator 추가

--- packages/server/src/product/entities/product.entity.ts
+++ packages/server/src/product/entities/product.entity.ts
@@ -2,6 +2,13 @@ import { Category } from '@category/entities/category.entity';
 import { BaseEntity } from '@src/base/BaseEntity';
 import { Location } from '@src/location/entities/location.entity';
 import { User } from '@src/user/entities/user.entity';
+import {
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 import { Column, Entity, JoinColumn, ManyToOne, Repository } from 'typeorm';
 
 enum status {
@@ -13,33 +20,42 @@ enum status {
 @Entity({ name: 'Product' })
 export class Product extends BaseEntity {
   @Column({ type: 'varchar', length: 255 })
+  @IsNotEmpty()
   title!: string;
 
   @Column({ type: 'text' })
+  @IsNotEmpty()
   description!: string;
 
   @Column({ type: 'decimal', precision: 10, scale: 0, default: 0 })
   price!: number;
 
   @Column({ type: 'json' })
-  imgUrl!: object;
+  @IsArray()
+  @IsString({ each: true })
+  imgUrl!: string[];
 
   @Column({ type: 'int', default: 0 })
+  @IsNumber()
   hits!: number;
 
   @Column({ type: 'enum', enum: status, default: status.onSale })
+  @IsEnum(status)
   status!: status;
 
   @ManyToOne(() => Location, (location) => location.id)
   @JoinColumn({ name: 'location_id' })
+  @IsNumber()
   locationId: Location;
 
   @ManyToOne(() => User, (user) => user.id)
   @JoinColumn({ name: 'seller_id' })
+  @IsNumber()
   sellerId: User;
 
   @ManyToOne(() => Category, (category) => category.name)
   @JoinColumn({ name: 'category_name', referencedColumnName: 'name' })
+  @IsString()
   categoryName: Category;
 }
 

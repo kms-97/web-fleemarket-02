30c95364ebed142b1815c4027c1b87de91da2c5b
kms-97 <msg016@naver.com>
2022-08-20 21:14:48 +0900

    feat: 상품 추가 메서드 구현 - 전달받은 카테고리명, 판매자id, 지역id를 검색하여 존재하지 않을 경우 404 에러를 반환합니다. - 전부 존재할 경우 해당 상품을 등록합니다.

--- packages/server/src/product/product.controller.ts
+++ packages/server/src/product/product.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ProductInsertDto } from './dto/productInsert.dto';
 import { ProductSearchDto } from './dto/productSearch.dto';
 import { ProductService } from './product.service';
 
@@ -9,5 +10,10 @@ export class ProductController {
   @Get()
   async getProducts(@Query() dto: ProductSearchDto) {
     return this.productService.findProduct(dto);
+  }
+
+  @Post()
+  async insertProduct(@Body() dto: ProductInsertDto) {
+    await this.productService.insertProduct(dto);
   }
 }
--- packages/server/src/product/product.service.ts
+++ packages/server/src/product/product.service.ts
@@ -1,16 +1,27 @@
+import { CategoryService } from '@category/category.service';
 import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CustomException } from '@src/base/CustomException';
 import { ErrorMessage } from '@src/constant/ErrorMessage';
-import { DataSource } from 'typeorm';
-import { Product } from './entities/product.entity';
+import { LocationService } from '@src/location/location.service';
+import { UserService } from '@src/user/user.service';
+import { ProductInsertDto } from './dto/productInsert.dto';
+import { ProductSearchDto } from './dto/productSearch.dto';
+import { Product, ProductRepository } from './entities/product.entity';
 
 const DEFAULT_LIMIT = 10;
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly datasource: DataSource) {}
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: ProductRepository,
+    private readonly categoryService: CategoryService,
+    private readonly userService: UserService,
+    private readonly locationService: LocationService,
+  ) {}
 
-  async findProduct(dto: any) {
+  async findProduct(dto: ProductSearchDto) {
     const { category, location } = dto;
     const page = dto.page ?? 1;
     let products: Product[];
@@ -31,13 +42,67 @@ export class ProductService {
     return { products, page };
   }
 
+  async insertProduct(dto: ProductInsertDto) {
+    const {
+      title,
+      description,
+      price,
+      imgUrl,
+      sellerId,
+      categoryName,
+      locationId,
+    } = dto;
+
+    const { category } = await this.categoryService.getCategoryByName(
+      categoryName,
+    );
+    if (!category) {
+      throw new CustomException(
+        [ErrorMessage.NOT_FOUND_TARGET('카테고리')],
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const { user } = await this.userService.getUserById(sellerId);
+    if (!user) {
+      throw new CustomException(
+        [ErrorMessage.NOT_FOUND_TARGET('사용자')],
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const { location } = await this.locationService.getLocationById(locationId);
+    if (!location) {
+      throw new CustomException(
+        [ErrorMessage.NOT_FOUND_TARGET('지역')],
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    await this.productRepository.query(
+      `
+      insert into Product (title, description, price, imgUrl, location_id, seller_id, category_name)
+      values (?, ?, ?, ?, ?, ?, ?)
+      `,
+      [
+        title,
+        description,
+        price,
+        JSON.stringify(imgUrl),
+        locationId,
+        sellerId,
+        categoryName,
+      ],
+    );
+  }
+
   private findProductByCategory(
     category: string,
     location: number,
     page: number,
   ) {
     const offset = (page - 1) * DEFAULT_LIMIT;
-    return this.datasource.query(
+    return this.productRepository.query(
       `
       select p.id as id, title, imgUrl, price, l.dong as locationName, category_name as categoryName,
         seller_id as sellerId, json_arrayagg(w.user_id) as likeUsers
@@ -54,7 +119,7 @@ export class ProductService {
 
   private findProductByLocation(location: number, page: number) {
     const offset = (page - 1) * DEFAULT_LIMIT;
-    return this.datasource.query(
+    return this.productRepository.query(
       `
       select p.id as id, title, imgUrl, price, l.dong as locationName, category_name as categoryName,
         seller_id as sellerId, json_arrayagg(w.user_id) as likeUsers

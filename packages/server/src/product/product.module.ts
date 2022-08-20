751e069b377b0acfec6f175280f9c93c2de23417
kms-97 <msg016@naver.com>
2022-08-20 21:14:18 +0900

    feat: product 모듈에서 다른 모듈 임포트

--- packages/server/src/product/product.module.ts
+++ packages/server/src/product/product.module.ts
@@ -1,11 +1,19 @@
+import { CategoryModule } from '@category/category.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LocationModule } from '@src/location/location.module';
+import { UserModule } from '@src/user/user.module';
 import { Product } from './entities/product.entity';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    CategoryModule,
+    UserModule,
+    LocationModule,
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })

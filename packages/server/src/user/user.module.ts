1ef0d79e9ac807bb73d691e027c4ec52d88c1475
kms-97 <msg016@naver.com>
2022-08-21 18:42:21 +0900

    feat: 유저 선호지역 핸들링을 위한 지역, 선호지역 모듈 추가

--- packages/server/src/user/user.module.ts
+++ packages/server/src/user/user.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LocationModule } from '@src/location/location.module';
+import { UserLocationModule } from '@src/userLocation/UserLocation.module';
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    LocationModule,
+    UserLocationModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],

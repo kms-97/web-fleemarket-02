3257e168a03d0874da54de75ff4862dd8a2af5fd
kms-97 <msg016@naver.com>
2022-08-22 12:34:21 +0900

    feat: 유저별 관심목록 조회 메서드 추가

--- packages/server/src/wish/wish.service.ts
+++ packages/server/src/wish/wish.service.ts
@@ -4,11 +4,30 @@ import { CustomException } from '@src/base/CustomException';
 import { ErrorMessage } from '@src/constant/ErrorMessage';
 import { Wish, WishRepository } from './entities/wish.entity';
 
+const DEFAULT_LIMIT = 10;
+
 @Injectable()
 export class WishService {
   constructor(
     @InjectRepository(Wish) private readonly wishRepository: WishRepository,
   ) {}
+
+  findWishByUserId(id: number, page: number) {
+    const offset = (page - 1) * DEFAULT_LIMIT;
+    return this.wishRepository.query(
+      `
+      select p.id as id, title, imgUrl, price, l.dong as locationName, category_name as categoryName,
+        seller_id as sellerId, json_arrayagg(w.user_id) as likeUsers
+      from wish w
+      left join product p on w.product_id = p.id
+      left join location l on p.location_id = l.id
+      where w.user_id = ?
+      group by p.id
+      limit ?, ?;
+      `,
+      [id, offset, DEFAULT_LIMIT],
+    );
+  }
 
   async insertWish(userId: number, productId: number) {
     await this.checkExistWish(userId, productId);

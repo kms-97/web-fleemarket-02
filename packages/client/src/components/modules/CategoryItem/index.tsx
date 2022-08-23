84e758aeec63e3a41a896477d5368060ba52cbe0
kms-97 <msg016@naver.com>
2022-08-23 17:45:09 +0900

    feat: Category Item 컴포넌트 추가 - 카테고리 데이터는 예시로 포함. - #35

---
+++ packages/client/src/components/modules/CategoryItem/index.tsx
@@ -0,0 +1,39 @@
+import Image from "@base/Image";
+import Text from "@base/Text";
+import styled from "@emotion/styled";
+import React from "react";
+
+interface Props {
+  category: any;
+  onClick: (category: string) => void;
+}
+
+const CategoryItem = ({ category, onClick }: Props) => {
+  const { name, img } = category;
+
+  return (
+    <StyledButton onClick={() => onClick(name)}>
+      <Image size="sm" src={img} />
+      <Text size="sm">{name}</Text>
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+
+  width: fit-content;
+  row-gap: 16px;
+
+  &:hover,
+  :focus {
+    & > p {
+      color: ${({ theme }) => theme.COLOR.PRIMARY1};
+    }
+  }
+`;
+
+export default CategoryItem;

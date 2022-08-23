e3d30bb7f8c78d08fb04da6ca6a93c07edb44b9e
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-23 12:25:33 +0900

    feat: BrowserRouter 적용
    - #31, #32

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import GlobalStyle from "@theme/GlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+import reportWebVitals from "./reportWebVitals";
+
+import App from "./App";
+import { theme } from "./theme";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle theme={theme} />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 reportWebVitals();

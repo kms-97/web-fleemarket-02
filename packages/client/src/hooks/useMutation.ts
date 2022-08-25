9334b6f3c3604b5d787a3aacfd640f59f508d84f
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-25 17:16:14 +0900

    feat: Create, Update, Delete 요청을 처리하는 useMutation 생성
    - #41

---
+++ packages/client/src/hooks/useMutation.ts
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+import { CacheOption, useCacheActionContext } from "@contexts/CacheContext";
+import { fetchReducer, IFetchInitialState, initialState } from "@src/reducers/fetchReducer";
+import { FETCH_FAILURE, FETCH_REQUEST, FETCH_SUCCESS } from "@constants/actions";
+
+const initialMutationOptions: CacheOption<any> = {
+  cacheClear: false,
+  onError: (error: string) => error,
+  onSuccess: (data: any) => data,
+};
+
+type MutationReturn<T> = [(...arg: any) => void, IFetchInitialState<T>];
+
+export const useMutation = <T>(
+  callback: (...arg: any) => T,
+  options: CacheOption<T> = initialMutationOptions,
+  keys: string[] = [],
+): MutationReturn<T> => {
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  const { onError, onSuccess, cacheClear } = {
+    ...initialMutationOptions,
+    ...options,
+  };
+  const { notify, clear } = useCacheActionContext();
+
+  const { data, error, loading } = state as IFetchInitialState<T>;
+
+  const mutate = async (...args: any) => {
+    if (cacheClear) {
+      clear();
+    }
+
+    dispatch({ type: FETCH_REQUEST });
+
+    try {
+      const result = await callback(...args);
+
+      onSuccess?.(result);
+      dispatch({ type: FETCH_SUCCESS, payload: result });
+      notify(keys);
+    } catch (error: any) {
+      onError?.(error.message);
+      dispatch({ type: FETCH_FAILURE, error: error.message });
+    }
+  };
+
+  return [mutate, { data, error, loading }];
+};

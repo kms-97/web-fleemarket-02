39c21a58a4fd2f31a5911b3230133aaf9c3c9751
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-26 10:04:06 +0900

    feat: 로그인한 유저 검증을 위한 HOC 생성
    - #43

---
+++ packages/client/src/hoc/auth.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { requestGetLoginUserInfo } from "@apis/auth";
+import { useQuery } from "@hooks/useQuery";
+
+const auth = (WrappedComponent: React.ComponentType) => {
+  const Component = () => {
+    const navigation = useNavigate();
+    const { data } = useQuery(["userinfo"], requestGetLoginUserInfo, {
+      onError() {
+        navigation("/login", { replace: true });
+      },
+      cacheExpiredTime: 1000 * 60 * 60,
+    });
+
+    return <WrappedComponent />;
+  };
+  return Component;
+};
+
+export default auth;

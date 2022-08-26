2194694c59d2a4c8e1cc66d7629da27a7244461d
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-26 10:00:50 +0900

    feat: token refresh 로직 생성
    - #43

--- packages/client/src/apis/index.ts
+++ packages/client/src/apis/index.ts
@@ -1,5 +1,9 @@
 import { BASE_URL } from "@constants/api";
 import { ICoreResponse } from "types/core.type";
+import { END_POINT } from "@constants/api";
+import { requestRefresh } from "./auth";
+
+const { REFRESH } = END_POINT;
 
 export const request = async <T extends ICoreResponse>(
   url: string,
@@ -29,10 +33,37 @@ export const request = async <T extends ICoreResponse>(
 
     return result;
   } catch (error: any) {
-    throw new Error(error.message);
+    if (error.message === "No auth token") {
+      const result = interceptors<T>(url, options);
+
+      return result;
+    } else {
+      throw new Error(error.message);
+    }
   }
 };
 
-// const authenticationInterceptor = (res: Response) => {
+const interceptors = async <T extends ICoreResponse>(
+  url: string,
+  options: RequestInit = {},
+): Promise<T> => {
+  try {
+    const refreshed = await request(REFRESH, { method: "POST" });
 
-// };
+    const { success, message } = refreshed;
+
+    if (!success) {
+      if (typeof message === "string") {
+        throw new Error(message);
+      } else {
+        throw new Error(message?.[0] ?? "");
+      }
+    }
+
+    const result = await request<T>(url, options);
+
+    return result;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};

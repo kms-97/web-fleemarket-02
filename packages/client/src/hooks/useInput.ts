d0f11d80eff248a78b4af33f266a17334ba522a5
kms-97 <msg016@naver.com>
2022-08-27 15:51:20 +0900

    feat: textArea props 타입 추가

--- packages/client/src/hooks/useInput.ts
+++ packages/client/src/hooks/useInput.ts
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 
 type ReturnType = [
   string,
-  (e: React.ChangeEvent<HTMLInputElement>) => void,
+  (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
   React.Dispatch<React.SetStateAction<string>>,
 ];
 
 export const useInput = (initialValue = ""): ReturnType => {
   const [value, setValue] = useState(initialValue);
 
-  const handler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setValue(e.target.value);
   };
 

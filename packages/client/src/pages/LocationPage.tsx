3516a05f4640b4a9503e62028e88a4897076e102
kms-97 <msg016@naver.com>
2022-08-28 14:17:06 +0900

    feat: 지역 설정 오류시 토스트 메세지 출력

--- packages/client/src/pages/LocationPage.tsx
+++ packages/client/src/pages/LocationPage.tsx
@@ -13,71 +13,79 @@ import Text from "@base/Text";
 import LocationForm from "@modules/LocationForm";
 import { IUserLocation } from "types/location.type";
 import styled from "@emotion/styled";
+import { useToastMessageAction } from "@contexts/ToastMessageContext";
 
 const LocationPage = auth(() => {
+  const { addToastMessage } = useToastMessageAction();
   const { data: user, updateCache } = useQuery(["userinfo"], requestGetLoginUserInfo);
-  const [deleteMutate] = useMutation(requestDeleteUserLocation, { cacheClear: true }, ["userinfo"]);
-  const [addMutate] = useMutation(requestAddUserLocation, { cacheClear: true }, ["userinfo"]);
-  const [updateMutate] = useMutation(requestUpdateUserLocation, { cacheClear: true }, ["userinfo"]);
+  const [deleteMutate] = useMutation(
+    requestDeleteUserLocation,
+    {
+      onError(error) {
+        addToastMessage({ type: "error", message: error, isVisible: true });
+      },
+      cacheClear: true,
+    },
+    ["userinfo"],
+  );
+  const [addMutate] = useMutation(
+    requestAddUserLocation,
+    {
+      onError(error) {
+        addToastMessage({ type: "error", message: error, isVisible: true });
+      },
+      cacheClear: true,
+    },
+    ["userinfo"],
+  );
+  const [updateMutate] = useMutation(
+    requestUpdateUserLocation,
+    {
+      onError(error) {
+        addToastMessage({ type: "error", message: error, isVisible: true });
+      },
+      cacheClear: true,
+    },
+    ["userinfo"],
+  );
 
   const addLocation = async (location: IUserLocation) => {
     if (!user) return;
 
     const { id } = location;
     const success = await addMutate({ locationId: id, userId: user?.id, isActive: true });
+    if (!success) return;
 
-    if (success === undefined) {
-      return;
-    }
-
-    if (success) {
-      const newUser = { ...user };
-      newUser.locations = newUser.locations.map((location) => {
-        location.isActive = false;
-        return location;
-      });
-      newUser.locations.push(location);
-      updateCache(["userinfo"], newUser);
-    } else {
-      console.log("");
-    }
+    const newUser = { ...user };
+    newUser.locations = newUser.locations.map((location) => {
+      location.isActive = false;
+      return location;
+    });
+    newUser.locations.push(location);
+    updateCache(["userinfo"], newUser);
   };
 
   const deleteUserLocation = async (locationId: number) => {
     if (!user) return;
     const success = await deleteMutate({ locationId });
+    if (!success) return;
 
-    if (success === undefined) {
-      return;
-    }
-
-    if (success) {
-      const newUser = { ...user };
-      newUser.locations = newUser.locations.filter((location) => location.id !== locationId);
-      updateCache(["userinfo"], newUser);
-    } else {
-      console.log("");
-    }
+    const newUser = { ...user };
+    newUser.locations = newUser.locations.filter((location) => location.id !== locationId);
+    updateCache(["userinfo"], newUser);
   };
 
   const activeUserLocation = async (locationId: number) => {
     if (!user) return;
     const success = await updateMutate({ locationId });
+    if (!success) return;
 
-    if (success === undefined) {
-      return;
-    }
-
-    if (success) {
-      const newUser = { ...user };
-      newUser.locations = newUser.locations.map((location) => {
-        location.isActive = location.id === locationId;
-        return location;
-      });
-      updateCache(["userinfo"], newUser);
-    } else {
-      console.log("");
-    }
+    const newUser = { ...user };
+    newUser.locations = newUser.locations.map((location) => {
+      location.isActive = location.id === locationId;
+      return location;
+    });
+    updateCache(["userinfo"], newUser);
   };
 
   if (!user) return <></>;

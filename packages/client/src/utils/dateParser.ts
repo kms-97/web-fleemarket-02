b0727804e7af46a99fe75bd0485bc8e91544dd7a
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-30 10:23:09 +0900

    feat: date Parser 변경

--- packages/client/src/utils/dateParser.ts
+++ packages/client/src/utils/dateParser.ts
@@ -1,10 +1,9 @@
-const amPm = ["오전", "오후"];
-
 export const getParsedDateWithHourMin = (createdAt: Date) => {
   const date = new Date(createdAt);
 
   const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
   const min = date.getMinutes();
+  const amPm = date.getHours() > 12 ? "오후" : "오전";
 
-  return `${amPm[Math.floor(hour % 12)]} ${hour}시 ${min}분`;
+  return `${amPm} ${hour}시 ${min}분`;
 };

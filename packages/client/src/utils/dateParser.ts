ea00737b020ac073f0f095e9e42407ee14ca6394
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-08-30 09:16:46 +0900

    fix: 한글 깨지는 문제 해결

--- packages/client/src/utils/dateParser.ts
+++ packages/client/src/utils/dateParser.ts
@@ -3,8 +3,8 @@ const amPm = ["오전", "오후"];
 export const getParsedDateWithHourMin = (createdAt: Date) => {
   const date = new Date(createdAt);
 
-  const hour = date.getHours() > 12 ? Math.floor(date.getHours() % 12) : date.getHours();
+  const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
   const min = date.getMinutes();
 
-  return `${amPm[Math.floor(hour / 12)]} ${hour}시 ${min}분`;
+  return `${amPm[Math.floor(hour % 12)]} ${hour}시 ${min}분`;
 };
